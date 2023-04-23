88bcc40760a66649a8be33e2e38ef33f40c420c1
Miri <mirifurman@gmail.com>
2023-04-23 21:54:44 +0300

    fix

--- client/src/api/Api.js
+++ client/src/api/Api.js
@@ -1,8 +1,11 @@
 import axios from 'axios'
+// import dotenv from 'dotenv';
 
 
-
-export let apiUrl = process.env.API_URL
+export let apiUrl = "http://localhost:5000/api";
+if(process.env.NODE_ENV==="production"){
+apiUrl = process.env.REACT_APP_API_URL
+}
 export const Api=axios.create({
     baseURL:apiUrl,
 })
